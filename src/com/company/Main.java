f8d2f0cc8195abddd2d679cf842e0c2f5bcf3511
sergey <sergeybushuev14@gmail.com>
2020-12-04 00:58:19 +0300

    first regs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here TODO
+    }
+    public String parser(String str) {
+        String mask = "Pipis pten4ik porter";
+
+        Pattern adressS = Pattern.compile("^0x[\\da-f]{10}:"); //Поиск адреса из начала строки, последнее что нужно
+        Pattern commandCheck = Pattern.compile("(\\bj[a-z]+)|(\\bcall[a-z]+)\\s+[^0]+");
+        return "";
     }
 }
