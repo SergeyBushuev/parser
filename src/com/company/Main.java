a41f46c551a80fc6bf1f34cae46feeacdc96c963
sergey <sergeybushuev14@gmail.com>
2020-12-04 01:00:53 +0300

    first regs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,6 @@ public class Main {
 	// write your code here TODO
     }
     public String parser(String str) {
-        String mask = "Pipis pten4ik porter";
 
         Pattern adressS = Pattern.compile("^0x[\\da-f]{10}:"); //Поиск адреса из начала строки, последнее что нужно
         Pattern commandCheck = Pattern.compile("(\\bj[a-z]+)|(\\bcall[a-z]+)\\s+[^0]+");
